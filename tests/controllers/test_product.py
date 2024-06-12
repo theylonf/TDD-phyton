8389744b3a68ce9ec1fc47e2f60387f5cd679a85
Theylon Feitosa <62394541+theylonf@users.noreply.github.com>
2024-06-12 18:52:13 +0600

    add some tests in test_product.py

--- tests/controllers/test_product.py
+++ tests/controllers/test_product.py
@@ -1,8 +1,11 @@
 from typing import List
 
 import pytest
+from pydantic import ValidationError
+from pytest import raises
 from tests.factories import product_data
 from fastapi import status
+from store.schemas.product import ProductIn
 
 
 async def test_controller_create_should_return_success(client, products_url):
@@ -51,16 +54,6 @@ async def test_controller_get_should_return_not_found(client, products_url):
         "detail": "Product not found with filter: 4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
     }
 
-
-@pytest.mark.usefixtures("products_inserted")
-async def test_controller_query_should_return_success(client, products_url):
-    response = await client.get(products_url)
-
-    assert response.status_code == status.HTTP_200_OK
-    assert isinstance(response.json(), List)
-    assert len(response.json()) > 1
-
-
 async def test_controller_patch_should_return_success(
     client, products_url, product_inserted
 ):
@@ -100,3 +93,51 @@ async def test_controller_delete_should_return_not_found(client, products_url):
     assert response.json() == {
         "detail": "Product not found with filter: 4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
     }
+
+async def test_valid_data_parses_correctly(self):
+    data = {
+        "name": "Iphone 14 Pro Max",
+        "quantity": 10,
+        "price": 8500,
+        "status": True,
+    }
+
+    product = ProductIn(**data)
+
+    assert product.name == "Iphone 14 Pro Max"
+    assert product.quantity == 10
+    assert product.price == 8500
+    assert product.status is True
+
+async def test_missing_required_field_raises_validation_error(self):
+        
+    data = {"name": "Iphone 14 Pro Max", "quantity": 10, "price": 8500}
+
+    with raises(ValidationError) as excinfo:
+        ProductIn(**data)
+
+    assert excinfo.value.errors() == [
+        {"loc": ("status",), "msg": "Field required", "type": "missing"}
+    ]
+
+async def test_invalid_price_type_raises_validation_error(self):
+    data = {
+        "name": "Iphone 14 Pro Max",
+        "quantity": 10,
+        "price": "8.500",
+        "status": True,
+    }
+
+    with raises(ValidationError) as excinfo:
+        ProductIn(**data)
+
+    assert "ensure they are of the correct types" in str(excinfo.value)
+
+
+@pytest.mark.usefixtures("products_inserted")
+async def test_controller_query_should_return_success(client, products_url):
+    response = await client.get(products_url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert isinstance(response.json(), List)
+    assert len(response.json()) > 1
